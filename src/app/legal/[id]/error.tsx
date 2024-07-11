d5026de27ff0aaa15048de3a26551504e227696f
Tejas013 <tejasmadhukar6@gmail.com>
2024-07-11 20:41:04 +0530

    Made dynamic page to show document by id

---
+++ src/app/legal/[id]/error.tsx
@@ -0,0 +1,9 @@
+"use client"
+export default function documentError({ error }: { error: Error & { digest?: string } }) {
+    return (
+        <>
+            <h1>Error fetching your document. The document with the given ID does not exsist.</h1>
+            <h1>{error.message}</h1>
+        </>
+    )
+}
