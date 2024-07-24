14996c3a751ba2dd906ecf2bcc6e8578325a5c9b
slaygun <nabhraghav@gmail.com>
2024-07-24 08:57:17 +0530

    form done!

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,18 +1,18 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { InputForm } from "./form";
 
 export default async function Home() {
     return (
         <main>
             <section className="flex min-h-screen">
                 <div className="lg:w-1/3 w-full flex justify-center items-center">
-                    <div className="">
+                    <div className="p-6">
                         <Image src="/assets/logo.png" alt="Thothica" width={180} height={180}/>
                         <h1 className="text-4xl font-bold text-gradient py-4">
                             Welcome to Thothica
                         </h1>
-                        
-                        <Button className="text-base font-bold w-full">Send Login Link</Button>
+
+                        <InputForm/>
                     </div>
                 </div>
                 <div className="lg:w-2/3 hidden lg:block relative">
