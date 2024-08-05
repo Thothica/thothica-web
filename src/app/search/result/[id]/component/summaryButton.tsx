0fbafe3168ca0cf64346095b57148757f90519ea
Tejas013 <tejasmadhukar6@gmail.com>
2024-08-05 23:58:30 +0530

    added query

--- src/app/search/result/[id]/component/summaryButton.tsx
+++ src/app/search/result/[id]/component/summaryButton.tsx
@@ -7,47 +7,50 @@ import { readStreamableValue } from "ai/rsc";
 import LargeInformation from "./largeInformation";
 
 export default function SummaryButton({
-  opensearchIndex,
-  dehydratedSource,
-  resultId,
+    opensearchIndex,
+    dehydratedSource,
+    resultId,
+    query
 }: {
-  opensearchIndex: Index;
-  dehydratedSource: string;
-  resultId: string;
+    opensearchIndex: Index;
+    dehydratedSource: string;
+    resultId: string;
+    query: string
 }) {
-  const [summary, setSummary] = useState("");
-  const [loading, setLoading] = useState(false);
+    const [summary, setSummary] = useState("");
+    const [loading, setLoading] = useState(false);
 
-  const HandleSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoading(true);
-    setSummary("Generating Summary, please wait a moment...");
-    const result = await generateSummary(
-      opensearchIndex,
-      dehydratedSource,
-      resultId,
+    const HandleSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+        setSummary("Generating Summary, please wait a moment...");
+        const result = await generateSummary(
+            opensearchIndex,
+            dehydratedSource,
+            resultId,
+            query
+        );
+
+        setSummary("");
+        // @ts-expect-error docs said to use this
+        for await (const content of readStreamableValue(result)) {
+            if (!content) continue;
+            setSummary(content);
+        }
+        setLoading(false);
+    };
+
+    return (
+        <>
+            {summary.length === 0 ? (
+                <form onSubmit={HandleSubmit}>
+                    <Button type="submit" disabled={loading}>
+                        {loading ? "Generating..." : "Generate Summary"}
+                    </Button>
+                </form>
+            ) : (
+                <LargeInformation title="Generated Summary" value={summary} />
+            )}
+        </>
     );
-
-    setSummary("");
-    // @ts-expect-error docs said to use this
-    for await (const content of readStreamableValue(result)) {
-      if (!content) continue;
-      setSummary(content);
-    }
-    setLoading(false);
-  };
-
-  return (
-    <>
-      {summary.length === 0 ? (
-        <form onSubmit={HandleSubmit}>
-          <Button type="submit" disabled={loading}>
-            {loading ? "Generating..." : "Generate Summary"}
-          </Button>
-        </form>
-      ) : (
-        <LargeInformation title="Generated Summary" value={summary} />
-      )}
-    </>
-  );
 }
